7a8afbc0c1278e1696fa91fd7fabd143d525eb77
seungje-yu <coffit23@gmail.com>
2023-11-03 13:11:51 +0900

    수정사항반영

--- src/main/java/com/ohgiraffers/employee/run/Application.java
+++ src/main/java/com/ohgiraffers/employee/run/Application.java
@@ -6,7 +6,7 @@ public class Application {
 
         /* 실행용 클래스 */
         /* 제공된 패키지에 필요한 클래스를 생성하고 코드를 기입 */
-        /* 데이터베이스 명 : memberdb
+        /* 데이터베이스 명 : employee
         *  아이디는 기존에 사용했던 ohgiraffers를 이용
         * */
     }
