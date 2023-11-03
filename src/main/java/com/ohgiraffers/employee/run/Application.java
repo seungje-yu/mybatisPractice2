722e9cf15a3caf4f6a2807e54c45d68b36428e50
seungje-yu <coffit23@gmail.com>
2023-11-03 13:05:50 +0900

    first commit

---
+++ src/main/java/com/ohgiraffers/employee/run/Application.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.employee.run;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        /* 실행용 클래스 */
+        /* 제공된 패키지에 필요한 클래스를 생성하고 코드를 기입 */
+        /* 데이터베이스 명 : memberdb
+        *  아이디는 기존에 사용했던 ohgiraffers를 이용
+        * */
+    }
+}
---
+++ src/main/java/com/ohgiraffers/employee/view/EmployeeView.java
@@ -0,0 +1,18 @@
+package com.ohgiraffers.employee.view;
+
+public class EmployeeView {
+
+    public void displayView() {
+
+        /*
+        ============ 멤버 관리 ====================
+        1. 멤버 전체 조회
+        2. 멤버 코드로 조회
+        3. 신규 멤버 추가
+        4. 멤버 수정
+        5. 멤버 삭제
+        ==========================================
+        멤버의 조회 결과의 결럼
+        */
+    }
+}
