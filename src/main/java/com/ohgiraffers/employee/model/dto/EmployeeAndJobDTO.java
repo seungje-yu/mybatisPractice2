a9a23852a4d23d75578e01d89fcb9296135974d3
JunKyeong-Lee <worldreaming@gmail.com>
2023-11-07 22:58:12 +0900

    [commit and push] 마지막 문제 이슈를 해결 했으나 옳은 답안은 아닌거 같다. 일단 제일 가능성 높은건, JobName 테이블 자체가 resultMap 에서 보이지 않았기 때문인데, 다만 JobDTO를 association 을 하면 오히려 더 갯수가 감소하는 현상을 보였음
    - 이준경
    - 20231107 22:56

--- src/main/java/com/ohgiraffers/employee/model/dto/EmployeeAndJobDTO.java
+++ src/main/java/com/ohgiraffers/employee/model/dto/EmployeeAndJobDTO.java
@@ -1,25 +1,67 @@
 package com.ohgiraffers.employee.model.dto;
 
 public class EmployeeAndJobDTO {
+    private String empName;
+    private String empNo;
+    private String email;
+    private String phone;
+    private String deptCode;
     private String jobName;
+    private String entYn;
     private String userType;
-    private EmployeeDTO employee;
 
     public EmployeeAndJobDTO() {
     }
 
-    public EmployeeAndJobDTO(EmployeeDTO employee, String jobName, String userType) {
-        this.employee = employee;
+    public EmployeeAndJobDTO(String empName, String empNo, String email, String phone, String deptCode, String jobName, String entYn, String userType) {
+        this.empName = empName;
+        this.empNo = empNo;
+        this.email = email;
+        this.phone = phone;
+        this.deptCode = deptCode;
         this.jobName = jobName;
+        this.entYn = entYn;
         this.userType = userType;
     }
 
-    public EmployeeDTO getEmployee() {
-        return employee;
+    public String getEmpName() {
+        return empName;
     }
 
-    public void setEmployee(EmployeeDTO employee) {
-        this.employee = employee;
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public String getEmpNo() {
+        return empNo;
+    }
+
+    public void setEmpNo(String empNo) {
+        this.empNo = empNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDeptCode() {
+        return deptCode;
+    }
+
+    public void setDeptCode(String deptCode) {
+        this.deptCode = deptCode;
     }
 
     public String getJobName() {
@@ -28,6 +70,14 @@ public class EmployeeAndJobDTO {
 
     public void setJobName(String jobName) {
         this.jobName = jobName;
+    }
+
+    public String getEntYn() {
+        return entYn;
+    }
+
+    public void setEntYn(String entYn) {
+        this.entYn = entYn;
     }
 
     public String getUserType() {
@@ -40,14 +90,7 @@ public class EmployeeAndJobDTO {
 
     @Override
     public String toString() {
-        return employee.getEmpName() + "|\t" +
-                employee.getEmpNo() + "|\t" +
-                employee.getEmail() + "|\t" +
-                employee.getPhone() + "|\t" +
-                employee.getDeptCode() + "|\t" +
-                jobName + "|\t" +
-                employee.getEntYn() + "|\t" +
-                userType;
+        return String.format("%s | %s | %20s | %12s | %2s | %3s | %s | %3s", empName, empNo, email, phone, deptCode, jobName, entYn, userType);
 
     }
 }
